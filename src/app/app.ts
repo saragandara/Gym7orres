b812f36e25ed5bce8504ce4d97f833facf95d070
‘Sara <sgandara@minsait.com>
2025-12-16 13:31:12 +0100

    feat: firebase configuration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,10 @@
+
 import { Component, effect, inject, signal, computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { JsonPipe } from '@angular/common';
+
+import { initializeApp } from "firebase/app";
+
 import { CdkDragDrop, CdkDropList, CdkDrag, CdkDropListGroup, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 import {MatCardModule} from '@angular/material/card';
@@ -24,5 +28,23 @@ import { NavigationComponent } from './components/navigation/navigation.componen
   styleUrl: './app.scss',
 })
 export class App {
+
+  ngOnInit() {
+    // Project Console: https://console.firebase.google.com/project/gym7orres/overview
+    // Hosting URL: https://gym7orres.web.app
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyD6r-oQJG8wZKNTw3D6gd4t54lCdokt6p4",
+      authDomain: "gym7orres.firebaseapp.com",
+      projectId: "gym7orres",
+      storageBucket: "gym7orres.firebasestorage.app",
+      messagingSenderId: "498794585388",
+      appId: "1:498794585388:web:9a40d7a994f9095a07e089"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+  }
+
   
 }
